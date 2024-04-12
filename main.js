1905044fe4b8fe7aadc681a273bc13a02e34eb79
Ana Alura <105944221+anaaxys@users.noreply.github.com>
2024-04-12 08:18:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-const botoes = document.querySelectorAll(."botao");
-console.log(botoes)
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0; i <botoes.length;ii+){
+    botoes[i].onclick = function (){
+        botoes[i].classList.remove
+    }
+}
